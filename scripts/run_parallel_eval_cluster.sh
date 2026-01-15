b2b3c6683b45d133f415712175e31bc8d405ffbd
David Louapre <david.louapre@huggingface.co>
2026-01-15 08:17:27 +0000

    New run of evals hash fixed, test 3 rules

--- scripts/run_parallel_eval_cluster.sh
+++ scripts/run_parallel_eval_cluster.sh
@@ -5,9 +5,9 @@
 #SBATCH --partition=hopper-cpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
-#SBATCH --time=23:59:59
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=01:59:59
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
